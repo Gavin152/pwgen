fc656542548b4bf02f9aa6c1112fc628df5a7244
niko <nikolas.molinari@gmail.com>
2024-07-28 22:10:17 +0200

    add character set test

--- pkg/random/random_test.go
+++ pkg/random/random_test.go
@@ -1,9 +1,9 @@
 package random
 
 import (
+	"regexp"
 	"strconv"
 	"testing"
-
 )
 
 func TestDigitCount(t *testing.T) {
@@ -23,4 +23,76 @@ func TestRandomness(t *testing.T) {
 	if rNum_01 == rNum_02 {
 		t.Fatalf(`Random int 1 was equal to int 2. %d == %d`, rNum_01, rNum_02)
 	}
+}
+
+func TestCharacterSet(t *testing.T) {
+	type scenario struct {
+		matchString string
+		options     RandomOptions
+	}
+	scenarios := make(map[string]scenario)
+	scenarios["LowerCaseOnly"] = scenario{
+		matchString: `^[a-z]+$`,
+		options: RandomOptions{
+			Lower:   true,
+			Upper:   false,
+			Numbers: false,
+			Symbols: false,
+		},
+	}
+	scenarios["UpperCaseOnly"] = scenario{
+		matchString: `^[A-Z]+$`,
+		options: RandomOptions{
+			Lower:   false,
+			Upper:   true,
+			Numbers: false,
+			Symbols: false,
+		},
+	}
+	scenarios["NumberOnly"] = scenario{
+		matchString: `^[0-9]+$`,
+		options: RandomOptions{
+			Lower:   false,
+			Upper:   false,
+			Numbers: true,
+			Symbols: false,
+		},
+	}
+	scenarios["SymbolOnly"] = scenario{
+		matchString: `^[!#$%^*()_+-={}[]|;<>,.?]+$`,
+		options: RandomOptions{
+			Lower:   false,
+			Upper:   false,
+			Numbers: false,
+			Symbols: true,
+		},
+	}
+	scenarios["UpperAndLower"] = scenario{
+		matchString: `^[a-zA-Z]+$`,
+		options: RandomOptions{
+			Lower:   true,
+			Upper:   true,
+			Numbers: false,
+			Symbols: false,
+		},
+	}
+	scenarios["UpperAndNumbers"] = scenario{
+		matchString: `^[A-Z0-9]+$`,
+		options: RandomOptions{
+			Lower:   false,
+			Upper:   true,
+			Numbers: true,
+			Symbols: false,
+		},
+	}
+
+	for key, scenario := range scenarios {
+		charSet := inputChars(&scenario.options)
+		found, _ := regexp.MatchString(scenario.matchString, charSet)
+		if !found {
+			t.Logf("Character set is invalid for %s: %s", key, charSet)
+			t.Fail()
+		}
+	}
+
 }
