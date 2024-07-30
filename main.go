1d5713d73eee6ce93d441c80d5aeb59d70f8cc0a
Gavin152 <nikolas.molinari@gmail.com>
2024-07-30 21:51:16 +0200

    remove doc generating code snippet

--- main.go
+++ main.go
@@ -4,16 +4,9 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
-	"log"
 	"pwgen/cmd"
-
-	"github.com/spf13/cobra/doc"
 )
 
 func main() {
 	cmd.Execute()
-	err := doc.GenMarkdownTree(cmd.RootCmd, "./")
-	if err != nil {
-		log.Fatal(err)
-	}
 }
